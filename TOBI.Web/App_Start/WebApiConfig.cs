3bd35c9dc3e5173ce90a1724edcbcc93d5dc4966
DESKTOP-210I7D2\HAILEVAN-PC <vanhaiit2018@gmail.com>
2018-10-16 19:29:05 +0700

    fix bug

--- TOBI.Web/App_Start/WebApiConfig.cs
+++ TOBI.Web/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace TOBI.Web
 {
@@ -10,7 +11,7 @@ namespace TOBI.Web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors(new EnableCorsAttribute(origins: "*", headers: "*", methods: "*"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
